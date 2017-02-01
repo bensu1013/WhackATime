d8f15edee18ea85958cb40c889f71384aa55903d
Benjamin Su <bensu1013@hotmail.com>
2017-02-01 13:57:35 -0500

    initial set up of views

---
+++ WhackATime/WhackATime/Droplets.swift
@@ -0,0 +1,15 @@
+//
+//  Droplets.swift
+//  WhackATime
+//
+//  Created by Benjamin Su on 2/1/17.
+//  Copyright © 2017 Benjamin Su. All rights reserved.
+//
+
+import UIKit
+
+class Droplets: UIView {
+
+    
+
+}
