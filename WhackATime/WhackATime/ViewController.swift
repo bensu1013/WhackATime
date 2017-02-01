32a7fe8f9d78d6b5c5ce18095c398c3a20a38316
Benjamin Su <bensu1013@hotmail.com>
2017-02-01 13:27:48 -0500

    Loaded Projected

---
+++ WhackATime/WhackATime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhackATime
+//
+//  Created by Benjamin Su on 2/1/17.
+//  Copyright © 2017 Benjamin Su. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
