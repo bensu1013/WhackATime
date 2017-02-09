c5dcd598af4c1131062752cec6aa999925d81d41
Benjamin Su <bensu1013@hotmail.com>
2017-02-09 13:32:47 -0500

    added textures in asset file

--- WhackATime/WhackATime/RainFactory.swift
+++ WhackATime/WhackATime/RainFactory.swift
@@ -13,6 +13,19 @@ class RainFactory {
     
     static var droplets = SKNode()
     
+    private static var dropTextures = [
+        SKTexture(imageNamed: "elephant"),
+        SKTexture(imageNamed: "giraffe"),
+        SKTexture(imageNamed: "hippo"),
+        SKTexture(imageNamed: "monkey"),
+        SKTexture(imageNamed: "panda"),
+        SKTexture(imageNamed: "parrot"),
+        SKTexture(imageNamed: "penguin"),
+        SKTexture(imageNamed: "pig"),
+        SKTexture(imageNamed: "rabbit"),
+        SKTexture(imageNamed: "snake")
+    ]
+    
     static func createRainFall(with delay: Double) -> SKAction {
         
         let timeDelay = SKAction.wait(forDuration: delay)
@@ -23,7 +36,10 @@ class RainFactory {
     
     private static func createDroplet() {
         
-        let drop = Droplet(texture: nil, color: UIColor.blue, size: CGSize(width: 64, height: 64))
+        let index = Int(arc4random_uniform(UInt32(dropTextures.count)))
+        let texture = dropTextures[index]
+        
+        let drop = Droplet(texture: texture, color: UIColor.clear, size: CGSize(width: texture.size().width / 4 , height: texture.size().height / 4))
         let x = CGFloat(arc4random_uniform(UInt32(1320))) - 660
         drop.position = CGPoint(x: x, y: 400)
         droplets.addChild(drop)
@@ -36,7 +52,7 @@ class RainFactory {
         
         for _ in 0...Int(randomSplash) {
             
-            let splash = Splash(texture: nil, color: UIColor.blue, size: CGSize(width: 16, height: 16))
+            let splash = Splash(texture: nil, color: UIColor.red, size: CGSize(width: 16, height: 16))
             splash.position = point
             droplets.addChild(splash)
             splash.physicsBody?.applyImpulse(CGVector(dx: drand48() - 0.5, dy: drand48() + 0.5))
